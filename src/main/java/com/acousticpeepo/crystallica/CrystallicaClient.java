a78ad480e23e855ab987c43a32932caee63a8bd1
Rachdi Sami <rachdi.sami@gmail.com>
2022-09-02 11:36:05 +0100

    Project Setup Updated LICENSE

---
+++ src/main/java/com/acousticpeepo/crystallica/CrystallicaClient.java
@@ -0,0 +1,10 @@
+package com.acousticpeepo.crystallica;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CrystallicaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
